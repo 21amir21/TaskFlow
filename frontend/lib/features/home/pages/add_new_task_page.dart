370096b5bfa3057a2513aacfed53d6b278a2508c
Amir Osama <amirosama21@gmail.com>
2025-03-01 21:27:55 +0200

    feat: finishing core featureess!!!!!

--- frontend/lib/features/home/pages/add_new_task_page.dart
+++ frontend/lib/features/home/pages/add_new_task_page.dart
@@ -1,5 +1,9 @@
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/features/auth/cubit/auth_cubit.dart';
+import 'package:frontend/features/home/cubit/tasks_cubit.dart';
+import 'package:frontend/features/home/pages/home_page.dart';
 import 'package:intl/intl.dart';
 
 class AddNewTaskPage extends StatefulWidget {
@@ -16,6 +20,28 @@ class _AddNewTaskPageState extends State<AddNewTaskPage> {
   TextEditingController descriptionController = TextEditingController();
   DateTime selectedDate = DateTime.now();
   Color selectedColor = const Color.fromRGBO(246, 222, 194, 1);
+  final formKey = GlobalKey<FormState>();
+
+  void createNewTask() async {
+    if (formKey.currentState!.validate()) {
+      AuthLoggedIn user = context.read<AuthCubit>().state as AuthLoggedIn;
+      await context.read<TasksCubit>().createNewTask(
+        uid: user.user.id,
+        title: titleController.text.trim(),
+        description: descriptionController.text.trim(),
+        color: selectedColor,
+        token: user.user.token,
+        dueAt: selectedDate,
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,45 +71,85 @@ class _AddNewTaskPageState extends State<AddNewTaskPage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        // makes the page scrollable
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              TextFormField(
-                controller: titleController,
-                decoration: const InputDecoration(hintText: "Title"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                controller: descriptionController,
-                decoration: const InputDecoration(hintText: "Description"),
-                maxLines: 4,
-              ),
-              const SizedBox(height: 10),
-              ColorPicker(
-                heading: const Text("Select color"),
-                subheading: const Text("Select a different shade"),
-                onColorChanged: (Color color) {
-                  setState(() {
-                    selectedColor = color;
-                  });
-                },
-                color: selectedColor,
-                pickersEnabled: const {ColorPickerType.wheel: true},
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text(
-                  "Submit",
-                  style: TextStyle(color: Colors.white, fontSize: 18),
+      body: BlocConsumer<TasksCubit, TasksState>(
+        listener: (context, state) {
+          if (state is TasksError) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.error)));
+          } else if (state is AddNewTaskSuccess) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Task added successfully!")),
+            );
+            Navigator.pushAndRemoveUntil(
+              context,
+              HomePage.route(),
+              (_) => false,
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is TasksLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            // makes the page scrollable
+            child: SingleChildScrollView(
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: titleController,
+                      decoration: const InputDecoration(hintText: "Title"),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return "Title cannot be empty!";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: descriptionController,
+                      decoration: const InputDecoration(
+                        hintText: "Description",
+                      ),
+                      maxLines: 4,
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return "Description cannot be empty!";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    ColorPicker(
+                      heading: const Text("Select color"),
+                      subheading: const Text("Select a different shade"),
+                      onColorChanged: (Color color) {
+                        setState(() {
+                          selectedColor = color;
+                        });
+                      },
+                      color: selectedColor,
+                      pickersEnabled: const {ColorPickerType.wheel: true},
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: createNewTask,
+                      child: const Text(
+                        "Submit",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
--- frontend/lib/features/home/pages/home_page.dart
+++ frontend/lib/features/home/pages/home_page.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/core/constants/utils.dart';
+import 'package:frontend/features/auth/cubit/auth_cubit.dart';
+import 'package:frontend/features/home/cubit/tasks_cubit.dart';
 import 'package:frontend/features/home/pages/add_new_task_page.dart';
 import 'package:frontend/features/home/widgets/date_selector.dart';
 import 'package:frontend/features/home/widgets/task_card.dart';
+import 'package:intl/intl.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static MaterialPageRoute route() =>
       MaterialPageRoute(builder: (context) => const HomePage());
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    final user = context.read<AuthCubit>().state as AuthLoggedIn;
+    context.read<TasksCubit>().getAllTasks(token: user.user.token);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,37 +41,76 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          const DateSelector(),
-          Row(
-            children: [
-              Expanded(
-                child: TaskCard(
-                  color: const Color.fromRGBO(246, 222, 194, 1),
-                  headerText: "Hello!",
-                  descriptionText:
-                      "This is a new task! This is a new task! This is a new task! This is a new task! This is a new task! This is a new task! This is a new task!",
+      body: BlocBuilder<TasksCubit, TasksState>(
+        builder: (context, state) {
+          if (state is TasksLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (state is TasksError) {
+            return Center(child: Text(state.error));
+          }
+          if (state is GetTasksSuccess) {
+            // to filter tasks based on the date
+            final tasks =
+                state.tasks
+                    .where(
+                      (elem) =>
+                          DateFormat("d").format(elem.dueAt) ==
+                              DateFormat("d").format(selectedDate) &&
+                          selectedDate.month == elem.dueAt.month &&
+                          selectedDate.year == elem.dueAt.year,
+                    )
+                    .toList();
+
+            return Column(
+              children: [
+                DateSelector(
+                  selectedDate: selectedDate,
+                  onTap: (date) {
+                    setState(() {
+                      selectedDate = date;
+                    });
+                  },
                 ),
-              ),
-              Container(
-                height: 10,
-                width: 10,
-                decoration: BoxDecoration(
-                  color: strengthenColor(
-                    const Color.fromRGBO(246, 222, 194, 1),
-                    0.69,
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: tasks.length,
+                    itemBuilder: (context, index) {
+                      final task = tasks[index];
+                      return Row(
+                        children: [
+                          Expanded(
+                            child: TaskCard(
+                              color: task.color,
+                              headerText: task.title,
+                              descriptionText: task.description,
+                            ),
+                          ),
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                              color: strengthenColor(task.color, 0.69),
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Text(
+                              DateFormat.jm().format(task.dueAt),
+                              style: TextStyle(fontSize: 17),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
                   ),
-                  shape: BoxShape.circle,
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(12.0),
-                child: Text("10:00AM", style: TextStyle(fontSize: 17)),
-              ),
-            ],
-          ),
-        ],
+              ],
+            );
+          }
+          return const SizedBox();
+        },
       ),
     );
   }
