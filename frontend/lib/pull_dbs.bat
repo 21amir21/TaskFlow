63ebe33fd6f111b89a5ae6bf9eb5ce79fe9987b3
Amir Osama <amirosama21@gmail.com>
2025-04-30 17:12:13 +0300

    feat: fix pull_dbs.bat to correctly save tasks.db and remove obsolete database files

--- frontend/lib/pull_dbs.bat
+++ frontend/lib/pull_dbs.bat
@@ -3,7 +3,7 @@ echo Pulling latest auth.db...
 adb exec-out run-as com.example.frontend cat databases/auth.db > "C:\Users\Amir\Desktop\Studygded\Degree Year 3\Semester 2\Mobile Application Development\TaskFlow\frontend\lib\auth.db"
 
 echo Pulling latest tasks.db...
-adb exec-out run-as com.example.frontend cat databases/tasks.db > "C:\Users\Amir\Desktop\Studygded\Degree Year 3\Semester 2\Mobile Application Development\TaskFlow\frontend\lib\auth.db"
+adb exec-out run-as com.example.frontend cat databases/tasks.db > "C:\Users\Amir\Desktop\Studygded\Degree Year 3\Semester 2\Mobile Application Development\TaskFlow\frontend\lib\tasks.db"
 
 echo Done. Files saved to project lib folder.
 pause
